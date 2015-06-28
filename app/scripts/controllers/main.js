fac90ba233e28f37256da2e991f4617efa760599
David Jay <davidjay@samsungaccelerator.com>
2015-06-27 23:25:58 -0400

    Outlined stats, will need to set up server for cross-domain requests to finish.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,28 +11,57 @@ angular.module('cityadminApp')
   .controller('MainCtrl', function ($scope,$http) {
   	$scope.results = [];
   	$scope.search = function(query) {
+  		// Get search results.
   		$scope.results=[];
   		$http.jsonp('http://54.85.208.63:9200/cities/_search?q='+encodeURIComponent(query) + '&size=100&callback=JSON_CALLBACK').
   			success(function(data, status, headers, config) {
-  				console.log(status);
-  				console.log(data);
   				$scope.stats = {
   					hits: data.hits.total
   				}
   				for (var i=0; i < data.hits.hits.length; i++){
   					var result = data.hits.hits[i];
-  					console.log(result);
 	  				$scope.results.push( {
 						title: result._source.title,
 						description: getRelevantSentence(result._source.body.description, result._source.body.text,query),
 						url: result._source.url,
+						city:result._source.city_name
 					})		
   				};
 
   			}). 
   			error(function(data, status, headers, config) {
   				console.log(status);
-  			})
+  			});
+
+  		//TODO: Sort results by city.
+		// Get stats
+		// $scope.stats ={};
+		// var statsreq = {
+		//  method: 'POST',
+		//  url: 'http://54.85.208.63:9200/cities/_search?q='+encodeURIComponent(query) + '&search_type=count&callback=JSON_CALLBACK',
+		//  headers: {
+		//    'Content-Type': 'application/json'
+		//  },
+		//  data: { 
+		// 	  "aggs": {
+		// 	    "cities": {
+		// 	      "terms": {
+		// 	        "field": "city_code"
+		// 	      }
+		// 	    }
+		// 	  }
+		//     }
+		//  };
+
+		// $http.jsonp(statsreq)
+		// 	.success(function(data,status,headers,config) {
+		// 		console.log(status);
+		// 		console.log(data);
+		// 	})
+		// 	.error(function(data,status,headers,config) {
+		// 		console.log(status);
+		// 		console.log(data);
+		// 	});
   	};
 
   	var getRelevantSentence = function(description, body, query) {
